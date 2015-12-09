c0858e4957a15c02b7bd5d803e3770d445b8342a
mayukh42 <mayukh.mukherjee@gmail.com>
2015-12-10 02:39:52 +0530

    added prime generator, factorial trailing zeros

--- algo_numeric2.c
+++ algo_numeric2.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "arrayutils.h"
+
+#define MAX_ELEMENTS 128
 
 /** author: mayukh
  * github.com/mayukh42
@@ -62,6 +65,51 @@ int findSingleElement (int * xs, int size) {
 }
 
 
+/** trailingZerosInFact ()
+ * returns the number of trailing zeros in factorial of given number
+ * http://www.spoj.com/problems/FCTRL/
+ */
+int trailingZerosInFact (int n) {
+	int max_exp_5 = (int) (log (n * 1.0)/ log (5.0));
+	int max_power = (int) (pow (5.0, max_exp_5));
+	int num5 = 0;
+	while (max_power > 1) {
+		num5 += n/max_power;
+		max_power /= 5;
+	}
+	return num5;
+}
+
+
+/** primeGeneratr ()
+ * generate primes up to given number
+ * Sieve of Eratosthenes + Memoization
+ * # primes approzimated using Prime Number Theorem (PNT)
+ */
+int primeGeneratr (int upper, int num) {
+	if (upper < 2)
+		return 0;
+	int * primes = (int *) calloc (sizeof (int), num);
+	int last_idx = 0; primes[last_idx++] = 2;
+	for (int i = 3; i <= upper; i++) {
+		int is_prime = 1;
+		for (int j = 0; j < last_idx; j++) {
+			if (!(i % primes[j])) {
+				is_prime = 0;
+				break;
+			}
+		}
+		if (last_idx == num)
+			break;
+		if (is_prime)
+			primes[last_idx++] = i;
+	}
+	outArrInt (primes, last_idx);
+	free (primes);
+	return last_idx;
+}
+
+
 void testPlateau () {
 	int xs[] = {1,2,2,3,3,3,4,5,5,6,6,6,6,7};
 	int count = 14;
@@ -101,11 +149,35 @@ void testFindSingleElement () {
 }
 
 
+void testTrailingZerosInFact () {
+	int ns[] = {3,60,100,1024,23456,8735373};
+	int size = 6, tz = 0;
+	for (int i = 0; i < size; i++) {
+		tz = trailingZerosInFact (ns[i]);
+		printf ("number of trailing zeros in %d! = %d\n", ns[i], tz);
+	}
+}
+
+
+void testPrimality () {
+	int ns[] = {10,100,500,1000,2000,10000};
+	int size = 6;
+	for (int i = 0; i < size; i++) {		
+		int pnt_n = (int) (ns[i] * 1.3 / log (ns[i]));
+		printf ("n = %d. PNT num = %d\n", ns[i], pnt_n);
+		int last_idx = primeGeneratr (ns[i], pnt_n);
+		printf ("# primes = %d\n\n", last_idx);
+	}
+}
+
+
 void runTests() {
 	// testPlateau ();
 	// testMissingElement ();
 	// testInplaceReverse ();
-	testFindSingleElement ();
+	// testFindSingleElement ();
+	// testTrailingZerosInFact ();
+	testPrimality ();
 }
 
 int main() {
